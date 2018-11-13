41ba0410140545ef88d56d5bfa48b0577985bde7
Taylor McKinnon <mail@tmacs.space>
2018-11-13 15:23:54 -0800

    reenable tests

--- tests/zenko_tests/docker-entrypoint.sh
+++ tests/zenko_tests/docker-entrypoint.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 
-# EXIT_STATUS="0"
+EXIT_STATUS="0"
 
-# enter_and_run() {
-#     local old_cwd="$(pwd)"
-#     cd "$1"
-#     sh -c "$2"
-#     if [ "$?" -ne "0" ]; then
-#         EXIT_STATUS="1"
-#         echo "$2 have failed"
-#     fi
-#     cd "$old_cwd"
-# }
+enter_and_run() {
+    local old_cwd="$(pwd)"
+    cd "$1"
+    sh -c "$2"
+    if [ "$?" -ne "0" ]; then
+        EXIT_STATUS="1"
+        echo "$2 have failed"
+    fi
+    cd "$old_cwd"
+}
 
 echo 'Waiting for ceph'
 sh wait_for_ceph.sh zenko-ceph-ceph-in-a-box
 
 # # Setup our environment
-# python3 create_buckets.py
-# if [ "$?" -ne "0" ]; then
-#     exit 1
-# fi
+python3 create_buckets.py
+if [ "$?" -ne "0" ]; then
+    exit 1
+fi
 
 # # Run the tests
-# enter_and_run python_tests "./run.sh $PYTHON_ARGS"
-# enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_location_quota test_bucket_get_v2"
+enter_and_run python_tests "./run.sh $PYTHON_ARGS"
+enter_and_run node_tests "npm_chain.sh test_crr test_api test_crr_pause_resume test_location_quota test_bucket_get_v2"
 
-# exit "$EXIT_STATUS"
-
-echo "hello"
+exit "$EXIT_STATUS"
