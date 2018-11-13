3cbb5692fa8429785bb046cba83934bf6e87ee88
Taylor McKinnon <mail@tmacs.space>
2018-11-12 16:29:20 -0800

    Add wait_for_ceph to test pod

---
+++ tests/zenko_tests/wait_for_ceph.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script is needed because RADOS Gateway
+# will open the port before beginning to serve traffic
+# causing wait_for_local_port.bash to exit immediately
+
+if [ -z "$1" ]; then
+    EP='127.0.0.1:8001'
+else
+    EP="$1"
+fi
+
+echo 'Waiting for ceph'
+while [ -z "$(curl $EP 2>/dev/null)" ]; do
+    sleep 1
+    echo -n "."
+done
