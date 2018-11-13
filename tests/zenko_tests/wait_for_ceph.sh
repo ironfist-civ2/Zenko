e7b05d427dd55c175253313dd065da9cb94872a8
Taylor McKinnon <mail@tmacs.space>
2018-11-13 10:17:38 -0800

    Move waiting to Makefile

--- tests/zenko_tests/wait_for_ceph.sh
+++ tests/zenko_tests/wait_for_ceph.sh
@@ -4,12 +4,7 @@
 # will open the port before beginning to serve traffic
 # causing wait_for_local_port.bash to exit immediately
 
-if [ -z "$1" ]; then
-    EP='127.0.0.1:8001'
-else
-    EP="$1"
-fi
-
+EP="zenko-ceph-ceph-in-a-box"
 echo "Waiting for ceph at $EP"
 while [ -z "$(curl $EP 2>/dev/null)" ]; do
     sleep 1
