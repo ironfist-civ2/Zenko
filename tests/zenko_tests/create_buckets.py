7a15b83d54f604ee86d2d1eebeb543f1798d6c53
Taylor McKinnon <mail@tmacs.space>
2018-11-20 15:21:55 -0800

    create seperate fuzzbcuket and enabled versioning

--- tests/zenko_tests/create_buckets.py
+++ tests/zenko_tests/create_buckets.py
@@ -95,8 +95,13 @@ buckets = [
     get_env('DO_CRR_SRC_BUCKET_NAME', 'zenko-do-crr-src-bucket'),
     get_env('MULTI_CRR_SRC_BUCKET_NAME', 'zenko-multi-crr-src-bucket'),
     get_env('TRANSIENT_SRC_BUCKET_NAME', 'ci-zenko-transient-src-bucket'),
-    'fuzzbucket'
 ]
+
+if get_env('S3_FUZZER') is not None:
+    buckets += [
+        'fuzzbucket-ver',
+        'fuzzbucket-nonver'
+    ]
 
 s = Session(aws_access_key_id=ZENKO_ACCESS_KEY,
             aws_secret_access_key=ZENKO_SECRET_KEY)
@@ -125,6 +130,11 @@ if not created:
     sys.exit(1)
 else:
     _log.info('Created buckets')
+
+if get_env('S3_FUZZER') is not None:
+    _log.info('Enabling version for s3://fuzzbucket-ver')
+    bkt = s3client.Bucket('fuzzbucket-ver').Versioning().enable()
+
 
 # Wait for all containers to become ready
 wait_for_pods(False, TIMEOUT)
